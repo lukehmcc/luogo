6d32a83e2162acbf49151373d638cf17e24c0729
covalent <mail@lukehmcc.com>
2025-07-29 12:05:25 -0400

    fix: update pods to allow running on iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import Flutter
 import UIKit
-import workmanager
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,8 +8,6 @@ import workmanager
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    WorkmanagerPlugin.registerBGProcessingTask(withIdentifier: "luogo background location ping")
-    WorkmanagerPlugin.registerPeriodicTask(withIdentifier: "app.luogo.app.iOSBackgroundAppRefresh", frequency: NSNumber(value: 20 * 60))
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
